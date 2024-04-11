d8639ffffe9505b93cb0109f6e96ffc16ddea907
manudana11 <manueldanapallardo@gmail.com>
2024-04-11 20:27:13 +0200

    casi terminado

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ for (let i = 0; i < allImg.length; i++) {
 const allPar = document.getElementsByTagName('p');
 for (let i = 0; i < allPar.length; i++) {
     allPar[i].addEventListener('click', (e) => {
-        e.target.style.color = 'blue';
+        e.target.style.color = `#${getRandom(paletaColores.map(color => color.hex))}`;
         e.target.style.backgroundColor = 'green';
     })
 }
@@ -67,14 +67,14 @@ for (let i = 0; i < allImg.length; i++) {
 
 for (let i = 0; i < allPar.length; i++) {
     allPar[i].addEventListener('mouseover', (e) => {
-        e.target.style.color = 'red';
+        //e.target.style.color = 'red';
         e.target.style.backgroundColor = 'black'
     });
 };
 
 for (let i = 0; i < allPar.length; i++) {
     allPar[i].addEventListener('mouseout', (e) => {
-        e.target.style.color = 'black';
+        //e.target.style.color = 'black';
         e.target.style.backgroundColor = 'white'
     });
 };
@@ -112,14 +112,18 @@ for (let i = 0; i < allSect.length; i++) {
 
 // Crea una función de nombre getRandom que acepte un array con valores y devuelva uno de ellos de manera aleatoria.
 
-// const colors = ['red', 'blue', 'green']
+const paletaColores = [{"name":"Tea green","hex":"e2f1af","rgb":[226,241,175],"cmyk":[6,0,27,5],"hsb":[74,27,95],"hsl":[74,70,82],"lab":[93,-16,30]},{"name":"Battleship gray","hex":"928779","rgb":[146,135,121],"cmyk":[0,8,17,43],"hsb":[34,17,57],"hsl":[34,10,52],"lab":[57,2,9]},{"name":"Wenge","hex":"5e5768","rgb":[94,87,104],"cmyk":[10,16,0,59],"hsb":[265,16,41],"hsl":[265,9,37],"lab":[38,7,-9]},{"name":"Delft Blue","hex":"3a445d","rgb":[58,68,93],"cmyk":[38,27,0,64],"hsb":[223,38,36],"hsl":[223,23,30],"lab":[29,3,-16]},{"name":"Jordy Blue","hex":"91c4f2","rgb":[145,196,242],"cmyk":[40,19,0,5],"hsb":[208,40,95],"hsl":[208,79,76],"lab":[77,-5,-28]},{"name":"White","hex":"fefffe","rgb":[254,255,254],"cmyk":[0,0,0,0],"hsb":[120,0,100],"hsl":[120,100,100],"lab":[100,0,0]},{"name":"Mint green","hex":"e5fcf5","rgb":[229,252,245],"cmyk":[9,0,3,1],"hsb":[162,9,99],"hsl":[162,79,94],"lab":[97,-9,1]},{"name":"Celadon","hex":"b3dec1","rgb":[179,222,193],"cmyk":[19,0,13,13],"hsb":[140,19,87],"hsl":[140,39,79],"lab":[85,-20,10]},{"name":"Dark purple","hex":"210124","rgb":[33,1,36],"cmyk":[8,97,0,86],"hsb":[295,97,14],"hsl":[295,95,7],"lab":[4,21,-15]},{"name":"Claret","hex":"750d37","rgb":[117,13,55],"cmyk":[0,89,53,54],"hsb":[336,89,46],"hsl":[336,80,25],"lab":[25,45,4]}];
 
-// const getRandom = (array) => {
-//     /* código misterioso */
-// }
+const colors = ['red', 'blue', 'green']
 
-// console.log(getRandom(colors))
-// // imprime 'red', 'blue' o 'green'
+const getRandom = (array) => {
+    const color = Math.floor(Math.random() * array.length);
+    return array[color];
+}
+
+console.log(getRandom(colors));
+// imprime 'red', 'blue' o 'green'
+
 // Utiliza la función creada getRandom para utilizar colores aleatorios de una paleta que hayas escogido de coolors.co en los apartados anteriores.
 
 // Utiliza la función creada getRandom para utilizar gifs aleatorios en los apartados anteriores.
